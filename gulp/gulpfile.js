e18146355753ff95697a3115931c83320d77c1a8
hypo9eal <uon@uonsMBA.local>
2016-01-29 17:05:36 +0900

    add gulp-sourcemaps

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,31 +1,37 @@
 'use strict';
 
-var gulp, sass, browserSync, autoprefixer;
+var gulp, sass, browserSync, autoprefixer, sourcemaps;
 
 gulp = require( 'gulp' );
 sass = require( 'gulp-sass' );
 browserSync = require( 'browser-sync' ).create();
-autoprefixer = require ( 'gulp-autoprefixer' );
+autoprefixer = require( 'gulp-autoprefixer' );
+sourcemaps = require( 'gulp-sourcemaps' );
 
+// task "css"
 gulp.task( 'css', function () {
   return gulp.src([
     '../source/**/*.scss'
   ])
+  .pipe( sourcemaps.init() )
   .pipe( sass().on( 'error', sass.logError ) )
   .pipe( autoprefixer() )
-  .pipe( gulp.dest( '../build' ))
-  .pipe( browserSync.stream());
+  .pipe( sourcemaps.write( 'map' ))
+  .pipe( gulp.dest( '../build' ) )
+  .pipe( browserSync.stream() );
 });
 
+// task "copy"
 gulp.task( 'copy', function () {
   return gulp.src([
     '../source/**/*',
-    '!../source/**/*.scss'
+    '!../source/css/**/*.scss'
   ])
   .pipe( gulp.dest( '../build' ) )
   .pipe( browserSync.stream());
 });
 
+// task "watch"
 gulp.task( 'watch', function () {
   browserSync.init({
     server: {
