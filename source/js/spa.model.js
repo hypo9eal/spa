8dc0c2b0567c475a28f35ba73977148a7c92fdb5
hypo9eal <uon@uonsMBA.local>
2016-02-10 17:59:49 +0900

    added chat object

--- source/js/spa.model.js
+++ source/js/spa.model.js
@@ -3,9 +3,11 @@
  * - spa.modelの定義（モデル）
  * - personオブジェクトの定義
  * - peopleオブジェクトの定義
+ * - chatオブジェクトの定義
  */
 
 /* eslint-env jquery, browser */
+/* eslint no-console: 1 */
 /* global TAFFY:false */
 
 spa.model = ( function () {
@@ -27,13 +29,15 @@ spa.model = ( function () {
      * @type {Number} cid_serial cidのシリアルナンバーの現在地
      * @type {Object} people_cid_map cidをkeyとしたpersonオブジェクトのコレクション
      * @type {Object} people_db DBオブジェクト
+     * @type {Bool} is_connected ユーザが現在チャットルームにいるか否か
      */
     stateMap = {
       user: null,
       anon_user: null,
       cid_serial: 0,
       people_cid_map: {},
-      people_db: TAFFY()
+      people_db: TAFFY(),
+      is_connected: false
     },
 
     jqueryMap = {
@@ -46,7 +50,7 @@ spa.model = ( function () {
     isFakeData = true,
 
     personProto, makeCid, clearPeopleDb, completeLogin,
-    makePerson, removePerson, people, initModule;
+    makePerson, removePerson, people, chat, initModule;
 
   /**
    * personオブジェクトのプロトタイプ
@@ -185,7 +189,7 @@ spa.model = ( function () {
     };
 
     /**
-     * DBオブジェクトを取得する
+     * DB上のpeopleオブジェクトを取得する
      * @return {Object} [description]
      */
     get_db = function () {
@@ -255,17 +259,116 @@ spa.model = ( function () {
   }() );
 
   /**
+   * public chatオブジェクト
+   * - チャットメッセージングを管理するためのメソッドとイベントを提供する
+   * - チャットルームの入退室
+   * - ユーザーリストが更新されたイベントのイベントリスナの割り当て
+   * - peopleオブジェクトの更新
+   * @return {Object} publicメソッドのマップ
+   */
+  chat = ( function () {
+    var
+      _publish_listchange, _update_list, _leave_chat, join_chat;
+
+    /**
+     * ユーザーリストを受け取り、peopleオブジェクトを更新する
+     * - ログインユーザーはcss_mapのみ更新し、ユーザーリストには追加しない
+     * - DBのユーザーをnameでソートする
+     * @param  {Array} arg_list 引数のリスト
+     * @return {[type]} [description]
+     */
+    _update_list = function ( arg_list ) {
+      var
+        i,
+        person_map,
+        make_person_map,
+        people_list = arg_list[ 0 ];
+
+      clearPeopleDb();
+
+      PERSON:
+      for( i = 0; i < people_list.length; i++ ) {
+        person_map = people_list[ i ];
+
+        if ( ! person_map.name ) { continue PERSON; }
+
+        if ( stateMap.user && stateMap.user.id === person_map._id ) {
+          stateMap.user.css_map = person_map.css_map;
+          continue PERSON;
+        }
+
+        make_person_map = {
+          cid: person_map._id,
+          css_map: person_map.css_map,
+          id: person_map._id,
+          name: person_map.name
+        };
+
+        makePerson( make_person_map );
+      }
+
+      stateMap.people_db.sort( 'name' );
+    };
+
+    /**
+     * peopleオブジェクトを更新し、イベントを発行する
+     * - _update_listを実行する
+     * - spa-listchangeイベントを発行する
+     * @param  {Array} arg_list [description]
+     * @return {[type]} [description]
+     */
+    _publish_listchange = function ( arg_list ) {
+      _update_list( arg_list );
+      jqueryMap.$document.trigger( 'spa-listchange', [ arg_list ] );
+    };
+
+    /**
+     * public チャットルームから退出する
+     */
+    _leave_chat = function () {
+      var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+      stateMap.is_connected = false;
+      if ( sio ) {
+        sio.emit( 'leavechat' );
+      }
+    };
+
+    /**
+     * public チャットルームに入室する
+     * - 匿名ユーザーを除外する
+     * - DBのlistchangeイベントにイベントハンドラを割りあてる
+     * @return {Bool} 入室したか否か
+     */
+    join_chat = function () {
+      var sio;
+
+      if ( stateMap.is_connected ) { return false; }
+
+      if ( stateMap.user.get_is_anon() ) {
+        console.warn( 'User must be defined before joining chat' );
+        return false;
+      }
+
+      sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+      sio.on( 'listchange', _publish_listchange );
+      stateMap.is_connected = true;
+
+      return true;
+    };
+
+    return {
+      _leave: _leave_chat,
+      join: join_chat
+    };
+  }());
+
+  /**
    * public モジュールの初期化をする
    * - 匿名ユーザーを作成して現在のユーザーとする
    * - DBからユーザーリストを取得する
    * @return {[type]} [description]
    */
   initModule = function () {
-    var
-      i,
-      people_list,
-      person_map;
-
     stateMap.anon_user = makePerson({
       cid: configMap.anon_id,
       id: configMap.anon_id,
@@ -273,23 +376,11 @@ spa.model = ( function () {
     });
 
     stateMap.user = stateMap.anon_user;
-
-    if ( isFakeData ) {
-      people_list = spa.fake.getPeopleList();
-      for (i = 0; i < people_list.length; i++ ) {
-        person_map = people_list[ i ];
-        makePerson({
-          cid: person_map._id,
-          css_map: person_map.css_map,
-          id: person_map._id,
-          name: person_map.name
-        });
-      }
-    }
   };
 
   return {
     initModule: initModule,
+    chat: chat,
     people: people
   };
 }());
