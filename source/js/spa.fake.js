120cee52eaa85708f7a4198c97c3f5b6599fcbff
hypo9eal <uon@uonsMBA.local>
2016-02-15 18:03:55 +0900

    added avater funstions

--- source/js/spa.fake.js
+++ source/js/spa.fake.js
@@ -82,7 +82,7 @@ spa.fake = ( function () {
      * @param  {[type]} data イベント発行時に渡された引数
      */
     emit_sio = function ( msg_type, data ) {
-      var person_map;
+      var person_map, i;
 
       /**
        * ユーザーがログインすると発行されるイベント
@@ -132,6 +132,21 @@ spa.fake = ( function () {
           listchange_idto = undefined;
         }
         send_listchange();
+      }
+
+      /**
+       * アバター機能でユーザー情報が更新されると発行されるイベント
+       * - 更新されたユーザーのアバター情報を更新する
+       * - 更新したユーザー情報を引数にlistchangeのコールバックを実行する
+       */
+      if ( msg_type === 'updateavatar' && callback_map.listchange ) {
+        for ( i = 0; i < peopleList.length; i++ ) {
+          if ( peopleList[ i ]._id === data.person_id ) {
+            peopleList[ i ].css_map = data.css_map;
+            break;
+          }
+        }
+        callback_map.listchange( [ peopleList ] );
       }
     };
 
