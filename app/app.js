b16e43e00a1491b0f9c88e739ed0523b4ade0939
hypo9eal <uon@uonsMBA.local>
2016-02-17 16:41:19 +0900

    initial commit of server

---
+++ app/app.js
@@ -0,0 +1,48 @@
+/**
+ * app.js
+ */
+
+/* eslint-env node */
+/* eslint no-console: 0 */
+
+'use strict';
+
+var
+  http = require( 'http' ),
+  express = require( 'express' ),
+  morgan = require( 'morgan' ),
+  bodyParser = require( 'body-parser' ),
+  methodOverride = require( 'method-override' ),
+  errorhandler = require( 'errorhandler' ),
+  app = express(),
+  server = http.createServer( app ),
+  env = process.env.NODE_ENV || 'development';
+
+app.use( bodyParser.json() );
+app.use( methodOverride('X-HTTP-Method-Override') );
+app.use( express.static( __dirname + '/build' ) );
+
+switch ( env ) {
+  case 'development':
+    app.use( morgan( 'combined' ) );
+    app.use( errorhandler( {
+      log: true
+    }) );
+    break;
+
+  case 'production':
+    app.use( errorhandler( {
+      log: false
+    }) );
+    break;
+}
+
+app.get( '/', function ( request, response ) {
+  response.redirect( '/spa.html' );
+});
+
+server.listen( 4000 );
+console.log(
+  'Node: Listening on port %d in %s mode',
+  server.address().port, app.settings.env
+);
